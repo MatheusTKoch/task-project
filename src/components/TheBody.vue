205a009f460dea5825d3ffef601981cf889f877c
Matheus <matheustrilhakoch@hotmail.com>
2023-06-29 20:41:58 -0300

    Added icons to components.

--- src/components/TheBody.vue
+++ src/components/TheBody.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <span
-      >{{ taskText }}
+    <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+  /> 
+  <span>-</span> 
+  <span
+      >{{ taskText }}<span class="material-symbols-outlined"> delete </span>
       <hr
     /></span>
   </div>
@@ -23,9 +28,14 @@ export default {
     },
     crossed: {
       type: Boolean,
-      required: true
-    }
+      required: true,
+    },
   },
 };
 </script>
 
+<style>
+.material-symbols-outlined {
+  font-variation-settings: "FILL" 0, "wght" 400, "GRAD" 0, "opsz" 48;
+}
+</style>
