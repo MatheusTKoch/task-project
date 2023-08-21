64c8aa1f65a08e3028e3c6a52a2a9c345380cf1e
Matheus <matheustrilhakoch@hotmail.com>
2023-08-21 20:35:24 -0300

    Adjusting login components

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,13 +1,37 @@
 <template>
   <form>
-    <div>
+    <!-- <div>
       <label for="username" class="font-medium text-xl" >Email: </label>
-      <input type="text" for="username" v-model="username" class="border-black"/>
+      <input type="text" for="username" v-model="username" class="border-black border-solid"/>
     </div>
     <div>
-      <label for="password" class="font-medium text-xl" min="6" max="15">Password: </label>
-      <input type="text" for="password" v-model="password" class="out-of-range:border-red-500 border-black"/>
-    </div>
+      <label for="password" class="font-medium text-xl" min="6" max="15">Password:</label>
+      <input type="password" for="password" v-model="password" class="out-of-range:border-red-500 border-black border-solid"/>
+    </div> -->
+  <div class="relative mb-3">
+  <input
+    type="text"
+    class="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-4 text-base font-normal leading-tight text-neutral-700 transition duration-200 ease-linear placeholder:text-transparent focus:border-primary focus:pb-[0.625rem] focus:pt-[1.625rem] focus:text-neutral-700 focus:outline-none peer-focus:text-primary dark:border-neutral-600 dark:text-neutral-200 dark:focus:border-primary dark:peer-focus:text-primary [&:not(:placeholder-shown)]:pb-[0.625rem] [&:not(:placeholder-shown)]:pt-[1.625rem]"
+    id="floatingInput"
+    placeholder="name@example.com" />
+  <label
+    for="floatingInput"
+    class="pointer-events-none absolute left-0 top-0 origin-[0_0] border border-solid border-transparent px-3 py-4 text-neutral-500 transition-[opacity,_transform] duration-200 ease-linear peer-focus:-translate-y-2 peer-focus:translate-x-[0.15rem] peer-focus:scale-[0.85] peer-focus:text-primary peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:scale-[0.85] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+    >Email address</label
+  >
+</div>
+<div class="relative mb-3">
+  <input
+    type="password"
+    class="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-4 text-base font-normal leading-tight text-neutral-700 transition duration-200 ease-linear placeholder:text-transparent focus:border-primary focus:pb-[0.625rem] focus:pt-[1.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none peer-focus:text-primary dark:border-neutral-600 dark:text-neutral-200 dark:focus:border-primary dark:peer-focus:text-primary [&:not(:placeholder-shown)]:pb-[0.625rem] [&:not(:placeholder-shown)]:pt-[1.625rem]"
+    id="floatingPassword"
+    placeholder="Password" />
+  <label
+    for="floatingPassword"
+    class="pointer-events-none absolute left-0 top-0 origin-[0_0] border border-solid border-transparent px-3 py-4 text-neutral-500 transition-[opacity,_transform] duration-200 ease-linear peer-focus:-translate-y-2 peer-focus:translate-x-[0.15rem] peer-focus:scale-[0.85] peer-focus:text-primary peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:scale-[0.85] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+    >Password</label
+  >
+</div>
     <span class="p-0.5">
       <base-button @click.prevent="emitUser" class="font-semibold">{{ buttonPrimary }}</base-button>
     </span>
