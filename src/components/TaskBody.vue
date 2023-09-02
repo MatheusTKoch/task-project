20626e3344135484b6f1688e96892f5c0da376a2
Matheus <matheustrilhakoch@hotmail.com>
2023-09-02 14:59:06 -0300

    Added and adjusting loading spinner

--- src/components/TaskBody.vue
+++ src/components/TaskBody.vue
@@ -5,12 +5,21 @@
       />
   <div class="dark:bg-gray-900">
     <main-header></main-header>
+    <div v-if="isLoading">
+    <div class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+      role="status">
+    <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+    >Loading...</span>
+    </div>
+    </div>
+    <div v-else>
     <div v-auto-animate>
     <ul v-for="(tasks, index) in taskArray" :key="index" @keydown.enter="pushTask" class="list-disc dark:text-white">
       <li class="p-3 font-normal text-2xl">
         {{ tasks.taskText }}<base-button><span class="material-symbols-outlined p-0" @click="deleteData(index)"> delete </span></base-button>
       </li>
     </ul>
+  </div>
   </div>
   </div>
 </template>
@@ -29,6 +38,7 @@ export default {
     const store = useStore();
     const taskArray = ref();
     const loggedUser = ref();
+    const isLoading = ref(false);
 
     function deleteData(data) {
       console.log(data)
@@ -41,18 +51,22 @@ export default {
     })
 
     function pushTask() {  
+      isLoading.value = true;
+      console.log(isLoading)
       var taskCountRef = firebase.database().ref("tasks");
       taskCountRef.orderByChild('userUID').equalTo(`${loggedUser.value}`).on("value", (snapshot) => {
         taskArray.value = snapshot.val();
       });
-      console.log(loggedUser.value)
+      isLoading.value = false;
+      console.log(isLoading)
       store.dispatch("refreshTasks");
     }
 
     return {
       taskArray,
       pushTask,
-      deleteData
+      deleteData,
+      isLoading
     };
   },
 };
